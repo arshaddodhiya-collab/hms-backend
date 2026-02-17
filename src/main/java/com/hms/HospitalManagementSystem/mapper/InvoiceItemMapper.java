ed1f9b815793985181bd04bd1703ea7aaed27bdb
arshaddodhiya-collab <arshad.dodhiya@artemhealthtech.com>
2026-02-17 14:47:26 +0530

    refactor: Add explicit mapping for `itemDescription` to `description` in `InvoiceItemMapper`.

--- src/main/java/com/hms/HospitalManagementSystem/mapper/InvoiceItemMapper.java
+++ src/main/java/com/hms/HospitalManagementSystem/mapper/InvoiceItemMapper.java
@@ -3,8 +3,10 @@ package com.hms.HospitalManagementSystem.mapper;
 import com.hms.HospitalManagementSystem.dto.response.InvoiceItemResponse;
 import com.hms.HospitalManagementSystem.entity.InvoiceItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface InvoiceItemMapper {
+    @Mapping(source = "itemDescription", target = "description")
     InvoiceItemResponse toResponse(InvoiceItem entity);
 }
